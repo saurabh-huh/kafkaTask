96214decf5a01901e91c068cb329fb47c22a5e8f
saurabh-huh <115425086+saurabh-huh@users.noreply.github.com>
2024-06-19 10:31:54 +0530

    Delete rideService directory

--- rideService/src/main/java/com/springboot/rideService/repository/RideRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.rideService.repository;
-
-import com.springboot.rideService.model.Ride;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface RideRepository extends MongoRepository<Ride, UUID> {
-}
