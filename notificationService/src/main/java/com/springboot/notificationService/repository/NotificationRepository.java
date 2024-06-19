35b89e9331297afd1d53e37c32d750bd0ef7391b
saurabh-huh <115425086+saurabh-huh@users.noreply.github.com>
2024-06-19 10:31:42 +0530

    Delete notificationService directory

--- notificationService/src/main/java/com/springboot/notificationService/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.notificationService.repository;
-
-import com.springboot.notificationService.model.Notification;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface NotificationRepository extends MongoRepository<Notification, UUID> {
-}
